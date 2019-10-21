1a2df7f3adc009bc073b958f440a62ec3315fb79
minternational <54006760+minternational@users.noreply.github.com>
2019-10-21 18:45:08 +0200

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+  
+  render() {
+    return(
+      <h2>Willkommen zu unserer Umfrage.</h2>
+    )
+  }
+
+}
+
+
+export default Header
