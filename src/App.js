dbde9a78068a9236f87cb28045f6c93c45774a3d
minternational <mintkuehn@gmail.com>
2019-10-29 19:36:01 +0100

    changed class component to function component and insert the correct react-router handling

--- src/App.js
+++ src/App.js
@@ -5,47 +5,22 @@ import Question from './components/Question'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      showComponent: false,
-    }
-    this.handeClick = this.handleClick.bind(this)
-  }
+function App() {
 
-  handleClick = () => {
-    this.setState({
-      showComponent: true })
-    // console.log("The button was clicked!")
-    // document.getElementById('page')
-    // document.getElementsByClassName('App-Header')
-  }    
-
-  
-  render() {
-    return (
+  return (
       <Router>
         <div className="App">
-          <Frontpage />
-            <Link to={'/components/Question'} className="nav-link"> Los geht's </Link>
           <Switch>
-            <Route path='/components/Question' component={Question} />
+            <Route path='/' exact >
+              <Frontpage />
+              <Link to={'/Question'} className="nav-link"><button id="button">Los geht's</button></Link>
+            </Route>
+            <Route path='/Question' component={Question} />
           </Switch>
         </div>
       </Router>        
-    );
-  }
+  );
 }
 
 
 export default App;
-
-
-
-// <header className="App-header">
-//     {this.state.showComponent ? <Question /> : <Frontpage />}
-//     <button id="button" onClick={this.handleClick}> {this.state.showComponent ? 'Weiter' : "Los geht's"}
-//     </button>
-//   </div>
-// </header>
